f000601540861c3fa87366af58dad6447e4085a0
Tomás Canavidez <tms1csgo@gmail.com>
2022-10-15 18:45:02 -0300

    update js

--- notitas/script.js
+++ notitas/script.js
@@ -1,22 +1,21 @@
-// info de fecha
-
+// Info date
 const dateNumber = document.getElementById('dateNumber');
 const dateText = document.getElementById('dateText');
 const dateMonth = document.getElementById('dateMonth');
 const dateYear = document.getElementById('dateYear');
 
-// contenedor de las tareas 
-const taskContainer = document.getElementById('taskContainer');
+// Tasks Container
+const tasksContainer = document.getElementById('tasksContainer');
 
 const setDate = () => {
-    const date = new date();
-    dateNumber.textContent = date.toLocaleString('es', { day: 'numeric'});
-    dateText.textContent = date.toLocaleString('es', {weekday: 'long'});
-    dateMonth.textContent = date.toLocaleString('es', {month: 'short'});
-    dateYear.textContent ? date.toLocaleString('es', {year: 'numeric'});
-}
+    const date = new Date();
+    dateNumber.textContent = date.toLocaleString('es', { day: 'numeric' });
+    dateText.textContent = date.toLocaleString('es', { weekday: 'long' });
+    dateMonth.textContent = date.toLocaleString('es', { month: 'short' });
+    dateYear.textContent = date.toLocaleString('es', { year: 'numeric' });
+};
 
-const addNewTask() = event => {
+const addNewTask = event => {
     event.preventDefault();
     const { value } = event.target.taskText;
     if(!value) return;
@@ -26,7 +25,7 @@ const addNewTask() = event => {
     task.textContent = value;
     tasksContainer.prepend(task);
     event.target.reset();
-}
+};
 
 const changeTaskState = event => {
     event.target.classList.toggle('done');
@@ -44,6 +43,5 @@ const order = () => {
 const renderOrderedTasks = () => {
     order().forEach(el => tasksContainer.appendChild(el))
 }
-
 
 setDate();
