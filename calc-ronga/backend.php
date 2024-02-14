aba8a5c0ab20be02ae9e4c9e35deb588f492c4c0
Tomás Canavidez <tms1csgo@gmail.com>
2024-02-14 09:32:43 -0300

    updating

--- calc-ronga/backend.php
+++ calc-ronga/backend.php
@@ -8,10 +8,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["precioContado"])) {
         "BANCARIZADA 6 CTAS" => ($precioContado * 1.355) / 6,
         "BANCARIZADA 3 SOLICITADO" => $precioContado * 1.18, 
         "BANCARIZADA 3 CTAS" => ($precioContado * 1.18) / 3,
-        "SUCREDITO 6 SOLICITADO" => $precioContado * 1.1799, 
-        "SUCREDITO 6 CTAS" => ($precioContado * 1.1799) / 6,
-        "SUCREDITO 3 SOLICITADO" => $precioContado * 1.1399, 
-        "SUCREDITO 3 CTAS" => ($precioContado * 1.1399) / 3,
+        // SUCREDITO 3 Y 6
+        // "SUCREDITO 6 SOLICITADO" => $precioContado * 1.1799, 
+        // "SUCREDITO 6 CTAS" => ($precioContado * 1.1799) / 6,
+        // "SUCREDITO 3 SOLICITADO" => $precioContado * 1.1399, 
+        // "SUCREDITO 3 CTAS" => ($precioContado * 1.1399) / 3,
+        
     );
 
     echo json_encode($cotizaciones);
